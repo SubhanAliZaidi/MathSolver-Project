260b881ae989bfbebc1703fd4ff5c3246f303d5f
Subhan Ali <Subhanali7897928120@gmail.com>
2023-05-29 19:12:44 +0530

    Static Files Folder Path Added

--- djangowork/settings.py
+++ djangowork/settings.py
@@ -125,10 +125,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'staticfiles')
 ]
 
 # Default primary key field type
