5bbdcc409559394cedc6b41bf6d05a0d7e375ac4
Subhan Ali <Subhanali7897928120@gmail.com>
2023-05-29 19:50:46 +0530

    Static fixed

--- djangowork/settings.py
+++ djangowork/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = 'static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 # Default primary key field type
